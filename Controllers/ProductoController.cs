7986fb16ea561fc7a908f97c785fe279b7bc468d
FirebirdCS <alvaritotutos@gmail.com>
2025-08-23 18:57:37 -0600

    Basic CRUD for API done

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -23,5 +23,57 @@ namespace WebApi.Controllers
             }
             return Ok(lista);
         }
+
+        [HttpGet("{id}", Name = "GetProducto")]
+        public async Task<ActionResult<Producto>> GetProducto(string id)
+        {
+            var producto = await DbContext.Producto.FirstOrDefaultAsync(p => p.ProductoId == id);
+            if (producto == null)
+            {
+                return new NoContentResult();
+            }
+            return Ok(producto);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Producto>> Post([FromBody] Producto value)
+        {
+            DbContext.Producto.Add(value);
+            await DbContext.SaveChangesAsync();
+            return new CreatedAtRouteResult("GetProducto", new { id = value.ProductoId }, value);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteProducto")]
+
+        public async Task<ActionResult<Producto>> Delete(string id)
+        {
+            Producto producto = await DbContext.Producto.FirstOrDefaultAsync(p => p.ProductoId == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                DbContext.Producto.Remove(producto);
+                await DbContext.SaveChangesAsync();
+                return producto;
+            }
+        }
+
+        [HttpPut("{id}", Name = "UpdateProducto")]
+
+        public async Task<ActionResult<Producto>> Put(string id, [FromBody] Producto value)
+        {
+            Producto producto = await DbContext.Producto.FirstOrDefaultAsync(p => p.ProductoId == id);
+            if (producto == null)
+            {
+                return BadRequest();
+            }
+            producto.Nombre = value.Nombre;
+            DbContext.Entry(producto).State = EntityState.Modified;
+            await DbContext.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
